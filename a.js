04548a4adc1effdc3d123e83783eaf01381c8d0e
Akash rawal <rawalakash37@gmail.com>
2021-04-15 17:13:29 +0530

    code01

---
+++ a.js
@@ -0,0 +1,30 @@
+console.log("Aakash rawal");
+console.log("Vikas Rawal");
+
+//Variable Decleration
+let x =15;
+let y =16;
+console.log(x+y);
+
+//function declaration
+function Name(){
+    console.log("My Name is AKash");  //Statement
+}
+//function calling
+Name();
+ function Nickname(){
+     console.log("Rawal"); //statement
+ }
+//function calling
+Nickname();
+ //fuction defiation
+ function Address(){
+     console.log("From Neemuch"); //statement
+ }
+ //function Calling
+ Address();
+ function college(){
+     console.log("Gyanodaya NMh");
+ }
+ college();
+ 
