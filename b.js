d4301ec7bd4cca355bbfcc932737181ff13fff24
Akash rawal <rawalakash37@gmail.com>
2021-04-16 09:57:01 +0530

    b.js work

---
+++ b.js
@@ -0,0 +1,13 @@
+console.log("hello word");
+
+let x="Aakash";
+let y="rawal";
+let z="kanka";
+//fuctio defination
+function A(){
+    console.log("hye "+x);
+    console.log(y);
+    console.log("From "+z)
+}
+//function calling
+A();
